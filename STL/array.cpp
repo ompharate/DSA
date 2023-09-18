d97d16a8665b5022c4bffdd672259546249340ce
Om Pharate <ompharate31@gmail.com>
2023-09-18 20:16:46 +0530

    STL - Array Done

---
+++ STL/array.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<array>
+using namespace std;
+int main() {
+
+    // int basic[3] = {1,2,3,4} // basic array
+
+    // static array
+    array<int, 4> arr ={1,2,3,4};
+
+    int size = arr.size();
+    // cout<<size;
+    
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cout<<arr[i];
+    // }
+
+    // cout<<arr.at(1);    
+    // cout<<arr.at(3);    
+
+    cout<<arr.empty(); // 0
+
+    cout<<arr.front(); // 1
+    cout<<arr.back(); // 4
+
+    return 0;
+}
